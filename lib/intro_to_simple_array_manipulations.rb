732b1a0ed91c9c6f1d2c20db0bfb33f672f61c8a
Tobie Tsuzuki <tobie.tsuzuki@gmail.com>
2019-09-17 18:26:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,21 +1,27 @@
 def using_push(Array, String)
   array.push
 end
+
 def using_unshift(Array,String)
   array.unshift
 end
+
 def using_pop(Array)
 array.pop
 end
+
 def pop_with_args(Array)
 array.pop(2)
 end
+
 def using_shift(Array)
 array.shift
 end
+
 def shift_with_args(Array) 
   array.shift(2)
 end
+
 def using_concat(Array, Array)
   array.concat(Array)
 end
