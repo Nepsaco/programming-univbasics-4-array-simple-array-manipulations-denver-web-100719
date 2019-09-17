a7f530459e2858590693b63f8692179f2dcdefe5
Tobie Tsuzuki <tobie.tsuzuki@gmail.com>
2019-09-17 18:36:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,7 +27,7 @@ def using_concat(array, array2)
 end
 
 def using_insert(array, element)
-  array[4].insert(element)
+  array[3].insert(element)
 end
 
 def using_uniq(array)
@@ -43,5 +43,5 @@ def using_delete(array,string)
 end
 
 def using_delete_at(array, integer)
-  array.deleteat(integer)
+  array.delete_at(integer)
 end
